c9ab9bb0ef0de1db5865d68f7f7a0ede1720dac4
Petr Molchanov <p.molchanov@rogii.com>
2023-10-01 18:35:35 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
-        src={"/phone-mock.png"}
+        src={"./phone-mock.png"}
         alt={"phone mock"}
         priority
         width={300}
