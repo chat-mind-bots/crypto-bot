a91e7b6a50736e447f401601470224c9641fc9be
Petr Molchanov <p.molchanov@rogii.com>
2023-10-01 18:25:27 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+// /** @type {import('next').NextConfig} */
+// const nextConfig = { trailingSlash: true, output: "export" };
+//
+// module.exports = nextConfig;
 
-module.exports = nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {};
+  }
+
+  return { trailingSlash: true, output: "export" };
+};
