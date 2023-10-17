c360216a5a3d9924097d5a0235e7d0490ac2051e
Solpio <ssusolpio@gmail.com>
2023-10-17 17:18:54 +0400

    animate

--- src/app/components/animations/send-coin/index.tsx
+++ src/app/components/animations/send-coin/index.tsx
@@ -1,9 +1,8 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { DynamicImage } from "app/components/images/dynamic-image";
 import classNames from "classnames";
 import { animated, useSpring } from "@react-spring/web";
-import shuffleArray from "app/helpers/shuffle-array.helper";
 import IosMessage from "app/components/ui-kit/ios-message";
 
 const botMessages: Array<{
@@ -26,71 +25,68 @@ const botMessages: Array<{
 
 const SendCoin = () => {
   const sizes = "(max-width: 768px) 100%";
-  const [elems, setElems] = useState(botMessages);
-  const [lastElem, setLastElem] = useState(elems[0]);
-  const [animationStart, setAnimationStart] = useState(false);
 
-  const [initialBlock, blockApi] = useSpring(() => ({
-    config: { duration: 500 },
-    from: { y: 0, opacity: animationStart ? 0 : 1, width: 398 },
-  }));
-
-  console.log(initialBlock, blockApi);
-  const [copyBlock, copyBlockApi] = useSpring(() => ({
-    config: { duration: 500 },
-    from: { y: -20, opacity: 0.1, width: 350 },
-  }));
-
-  const handleStart = () => {
-    setAnimationStart(true);
-    blockApi.start({
-      to: [
-        // { y: 0, opacity: 1 },
-        {
-          y: -20,
-          opacity: 0.8,
-          width: 350,
-          onStart: handleContinue,
-          onRest: () => {
-            setLastElem(elems[0]);
-          },
-        },
-        {
-          y: -20,
-          opacity: 0,
-        },
-        {
-          y: 10,
-          opacity: 1,
-          width: 350,
-          immediate: true,
-          onStart: () => {
-            onShuffle();
-          },
-        },
-        { opacity: 1, width: 398 },
-      ],
-    });
-  };
-  const handleContinue = () => {
-    copyBlockApi.start({
-      to: [
-        { y: -20, opacity: 0, width: 350 },
-        { y: -20, opacity: 0.5, width: 350 },
-      ],
-    });
+  const initial = {
+    opacity: 1,
+    y: 0,
+    zIndex: 2,
   };
 
-  const onShuffle = () => {
-    setElems(shuffleArray(elems));
+  const hidden = {
+    y: -20,
+    scale: 0.9,
+    opacity: 0.2,
+    zIndex: 0,
+  };
+
+  const end = {
+    display: "block",
+    y: 20,
+    opacity: 0,
+    zIndex: -1,
+  };
+
+  const [firstBlock, apiFirstBlock] = useSpring(() => ({
+    from: { ...initial },
+    config: { duration: 1000 },
+  }));
+  const [secondBlock, apiSecondBlock] = useSpring(() => ({
+    from: { ...end },
+    config: { duration: 1000 },
+  }));
+  const [thirdBlock, apiThirdBlock] = useSpring(() => ({
+    from: { ...end },
+    config: { duration: 1000 },
+  }));
+
+  const styles = [firstBlock, secondBlock, thirdBlock];
+
+  const firstTick = () => {
+    apiFirstBlock.start({ to: { ...hidden } });
+    apiSecondBlock.start({ to: { ...initial } });
+    apiThirdBlock.start({ to: { ...end } });
+    secondTick();
+  };
+
+  const secondTick = () => {
+    apiFirstBlock.start({ to: { ...end }, delay: 2000 });
+    apiSecondBlock.start({ to: { ...hidden }, delay: 2000 });
+    apiThirdBlock.start({ to: { ...initial }, delay: 2000 });
+    thirdTick();
+  };
+
+  const thirdTick = () => {
+    apiFirstBlock.start({ to: { ...initial }, delay: 4000 });
+    apiSecondBlock.start({ to: { ...end }, delay: 4000 });
+    apiThirdBlock.start({ to: { ...hidden }, delay: 4000 });
   };
 
   useEffect(() => {
-    const interval = setInterval(handleStart, 2500);
+    const interval = setInterval(firstTick, 6000);
     return () => {
       clearInterval(interval);
     };
-  }, [elems, lastElem]);
+  }, [firstTick]);
   return (
     <div
       className={classNames(
@@ -118,114 +114,43 @@ const SendCoin = () => {
         )}
         name={"half-mockup"}
       />
-      <div className={classNames("absolute", "flex", "justify-center")}>
-        <animated.div
-          className={classNames(
-            "absolute",
-            "bottom-[0]",
-            "w-full",
-            "bottom-0",
-            "ml-[56px]",
-            "mr-[56px]",
-            "min-w-[212px]",
-            "max-w-[398px]",
-            "min-h-[76px]",
-            "max-h-[125px]",
-            "rounded-[20px]",
-            "bg-messageBgLight",
-            "dark:bg-messageBgDark",
-          )}
-          style={{
-            height: 125,
-            zIndex: 1,
-            ...initialBlock,
-          }}
-        >
-          {/* <ButtonText className={classNames("text-dark", "dark:text-white")}>*/}
-          {/*  {elems[0]}*/}
-          {/* </ButtonText>*/}
-          <IosMessage
-            title={"Crypto bot"}
-            description={elems[0].description}
-            icon={"message-icon"}
-            iconAlt={"ios message"}
-            secondaryDescription={elems[0].secondaryDescription}
-          />
-        </animated.div>
-        <animated.div
-          className={classNames(
-            "absolute",
-            "w-full",
-            "bottom-0",
-            "ml-[56px]",
-            "mr-[56px]",
-            "min-w-[212px]",
-            "max-w-[398px]",
-            "min-h-[76px]",
-            "max-h-[125px]",
-            "rounded-[20px]",
-            "bg-bgLight",
-            "dark:bg-bgDark",
-          )}
-          style={{
-            height: 125,
-            ...copyBlock,
-          }}
-        >
-          <IosMessage
-            title={"Crypto bot"}
-            description={lastElem.description}
-            icon={"message-icon"}
-            iconAlt={"ios message"}
-            secondaryDescription={lastElem.secondaryDescription}
-          />
-        </animated.div>
+      <div
+        className={classNames("absolute", "flex", "w-full", "justify-center")}
+      >
+        {botMessages.map((message, index) => (
+          <animated.div
+            key={message.description}
+            className={classNames(
+              "absolute",
+              "bottom-[0]",
+              "w-full",
+              "bottom-0",
+              "ml-[56px]",
+              "mr-[56px]",
+              "min-w-[212px]",
+              "max-w-[398px]",
+              "min-h-[76px]",
+              "max-h-[125px]",
+              "rounded-[20px]",
+              "bg-messageBgLight",
+              "dark:bg-messageBgDark",
+            )}
+            style={{
+              height: 125,
+              width: "100%",
+              ...styles[index],
+            }}
+          >
+            <IosMessage
+              title={"Crypto bot"}
+              description={message.description}
+              icon={"message-icon"}
+              iconAlt={"ios message"}
+              secondaryDescription={message.secondaryDescription}
+            />
+          </animated.div>
+        ))}
       </div>
-      {/* <Shuffler*/}
-      {/* className={classNames(*/}
-      {/*   "flex",*/}
-      {/*   "absolute",*/}
-      {/*   "top-0",*/}
-      {/*   "justify-center",*/}
-      {/*   "absolute",*/}
-      {/*   "w-full",*/}
-      {/*   "h-full",*/}
-      {/*   "pl-[35px]",*/}
-      {/*   "pr-[35px]",*/}
-      {/*   "mt-[20px]",*/}
-      {/*   "md:mt-0",*/}
-      {/*   "md:pl-[106px]",*/}
-      {/*   "md:pr-[106px]",*/}
-      {/* )}*/}
-      {/* direction={ShuffleDirectionEnum.VERTICAL}*/}
-      {/* isOpacityEnable={false}*/}
-      {/* isFixed={false}*/}
-      {/* >*/}
-      {/* {elems.map(({ messageName }, index) => {*/}
-      {/*   return (*/}
-      {/*     <div*/}
-      {/*       key={`send-coin--${messageName}`}*/}
-      {/*       ref={createRef()}*/}
-      {/*       className={classNames(*/}
-      {/*         index === elems.length - 1 ? "opacity-100" : "opacity-50",*/}
-      {/*       )}*/}
-      {/*       style={{*/}
-      {/*         top: `${index * 24}px`,*/}
-      {/*         zIndex: `${index}`,*/}
-      {/*       }}*/}
-      {/*     >*/}
-      {/*       <DynamicImage*/}
-      {/*         height={125}*/}
-      {/*         width={320}*/}
-      {/*         ext={"png"}*/}
-      {/*         sizes={sizes}*/}
-      {/*         alt={"Telegram message"}*/}
-      {/*         name={messageName}*/}
-      {/*       />*/}
-      {/*     </div>*/}
-      {/*   );*/}
-      {/* })}*/}
-      {/* </Shuffler>*/}
     </div>
   );
 };
